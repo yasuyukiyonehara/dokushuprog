a1a518a474447c66fcaf9915e23f41c8259508e7
yasuyukiyonehara <yasuprog@gmail.com>
2022-12-24 21:32:12 +0900

    組み込み関数に関してレッスン

---
+++ Chapter3/kumikomi_kansu/kumikomi_kansu_ex04.py
@@ -0,0 +1,20 @@
+n = input("Type a number: ")
+n = int(n)
+if n % 2 == 0:
+    print("n is even")
+else:
+    print("n is odd")
+
+n = input("Type a number: ")
+n = int(n)
+if n % 2 == 0:
+    print("n is even")
+else:
+    print("n is odd")
+
+n = input("Type a numvber: ")
+n = int(n)
+if n % 2 == 0:
+    print("n is even")
+else:
+    print("n is odd")
---
+++ Chapter3/kumikomi_kansu/kumikomi_kansu_ex05.py
@@ -0,0 +1,11 @@
+def even_odd():
+    n = input("Type a number: ")
+    n = int(n)
+    if n % 2 == 0:
+        print("n is even")
+    else:
+        print("n is odd")
+
+even_odd()
+even_odd()
+even_odd()
