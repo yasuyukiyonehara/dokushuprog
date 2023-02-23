ccc0284eeafdd9a4715e7d0138c2949020646af6
yasuyukiyonehara <yasuprog@gmail.com>
2023-02-23 16:27:25 +0900

    文字列の操作を書く

---
+++ Chapter6/string_manipulations_charenge01.py
@@ -0,0 +1,9 @@
+string = "カミュ"
+
+string_1 = string[0]
+string_2 = string[1]
+string_3 = string[2]
+
+print(string_1)
+print(string_2)
+print(string_3)
---
+++ Chapter6/string_manipulations_charenge02.py
@@ -0,0 +1,7 @@
+input_1 = input("何を書きましたか？：")
+input_2 = input("誰に送りましたか？：")
+
+words = ("私は昨日{}を書いて、{}に送った！").format(input_1,input_2)
+
+print(words)
+
